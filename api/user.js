f1f42652d776a98404c1a22f4bdbd45ea7810b8d
Brooke Zanone <bzanone04@gmail.com>
2025-10-02 11:03:34 -0400

    Created user.js services skeleton

---
+++ api/user.js
@@ -0,0 +1,84 @@
+// user.js
+// 12 Services
+
+// User structure:
+// firstName
+// lastName
+// userID
+// role
+// email
+// username
+// password
+
+// 1. Get a single user by ID (GET)
+// /api/user/:id
+app.get('/api/user/:id', (req, res) => {
+    
+});
+
+
+// 2. Create user (POST) 
+// /api/user/createuser
+app.post('/api/user/createuser', (req, res) => {
+
+});
+
+// 3. Update username (PUT)
+// /api/user/:id/updateusername
+app.put('/api/store/items/:id', (req, res) => {
+
+});
+
+// 4. Update user's name (PUT)
+// /api/user/:id/updatename
+app.put('/api/user/:id/updatename', (req, res) => {
+
+});
+
+// 5. Update user's email (PUT)
+// /api/user/:id/updateemail
+app.put('/api/user/:id/updateemail', (req, res) => {
+
+});
+
+// 6. Delete user's account (DELETE)
+// /api/user/:id
+app.delete('/api/store/items/:id', (req, res) => {
+
+});
+
+// 7. Update profile icon (PUT)
+// /api/user/:id/updateicon
+app.put('/api/user/:id/updateicon', (req, res) => {
+
+});
+
+// 8. Update profile banner (PUT)
+// /api/user/:id/updatebanner
+app.put('/api/user/:id/updatebanner', (req, res) => {
+
+});
+
+// 9. Update profile backdrop (PUT)
+// /api/user/:id/updatebackdrop
+app.put('/api/user/:id/updatebackdrop', (req, res) => {
+
+});
+
+// 10. Get available icons (GET)
+// /api/user/icons
+app.get('/api/user/icons', (req, res) => {
+    
+});
+
+// 11. Get available banners (GET)
+// /api/user/banners
+app.get('/api/user/banners', (req, res) => {
+    
+});
+
+// 12. Get available backdrops (GET)
+// /api/user/backdrops
+app.get('/api/user/backdrops', (req, res) => {
+    
+});
