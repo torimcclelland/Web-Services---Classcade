3cddef775b9c959030d8042bd17a3518a7bce3f0
ocelmo <tori9991@hotmailcom>
2025-10-02 11:30:23 -0400

    logic for calculating project progress
    based on added parameter of project goal time

--- models/project.js
+++ models/project.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const trackedTimeSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  timeSpent: { type: Number, default: 0 } // in minutes or hours
+  timeSpent: { type: Number, default: 0 }
 });
 
 const projectSchema = new mongoose.Schema({
@@ -12,6 +12,7 @@ const projectSchema = new mongoose.Schema({
   dueDate: { type: Date },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   trackedTime: [trackedTimeSchema],
+  goalTime: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
