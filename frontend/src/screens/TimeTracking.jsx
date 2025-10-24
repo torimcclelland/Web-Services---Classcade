e80b7fc3e1044cc698fb0582b31a5f39af993aa3
Brooke <bzanone04@gmail.com>
2025-10-24 12:38:42 -0400

    Set up TimeTracking screen and console logs

---
+++ frontend/src/screens/TimeTracking.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// Create stylesheet for this page and import here
+
+const TimeTracking = () => {
+    console.log('You are on the Time Tracking screen.')
+}
+
+const handleClassSelect = () => {
+    console.log('You are now selecting your class.')
+}
+
+const handleTaskSelect = () => {
+    console.log('You are now selecting your task.')
+}
+
+const handleMinutes = () => {
+    console.log('You are now entering your minutes.')
+}
+
+const handleCompletedStatus = () => {
+    console.log('You are now checking off the completed status.')
+}
+
+const handleCancel = () => {
+    console.log('You are now cancelling your time tracking.')
+}
+
+const handleSubmit = () => {
+    console.log('You are now submitting your time tracking.')
+}
