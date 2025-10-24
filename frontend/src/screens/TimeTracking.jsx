1c480d7a04d65bcc56825edb24eadb9b7f3cea1b
Brooke <bzanone04@gmail.com>
2025-10-24 12:48:21 -0400

    Set up buttons in TimeTracking

--- frontend/src/screens/TimeTracking.jsx
+++ frontend/src/screens/TimeTracking.jsx
@@ -3,28 +3,52 @@ import React from 'react';
 
 const TimeTracking = () => {
     console.log('You are on the Time Tracking screen.')
-}
 
-const handleClassSelect = () => {
-    console.log('You are now selecting your class.')
-}
 
-const handleTaskSelect = () => {
-    console.log('You are now selecting your task.')
-}
+    const handleClassSelect = () => {
+        console.log('You are now selecting your class.')
+    }
 
-const handleMinutes = () => {
-    console.log('You are now entering your minutes.')
-}
+    const handleTaskSelect = () => {
+        console.log('You are now selecting your task.')
+    }
 
-const handleCompletedStatus = () => {
-    console.log('You are now checking off the completed status.')
-}
+    const handleMinutes = () => {
+        console.log('You are now entering your minutes.')
+    }
 
-const handleCancel = () => {
-    console.log('You are now cancelling your time tracking.')
-}
+    const handleCompletedStatus = () => {
+        console.log('You are now checking off the completed status.')
+    }
 
-const handleSubmit = () => {
-    console.log('You are now submitting your time tracking.')
-}
+    const handleCancel = () => {
+        console.log('You are now cancelling your time tracking.')
+    }
+
+    const handleSubmit = () => {
+        console.log('You are now submitting your time tracking.')
+    }
+
+    return (
+        <div>
+        <h1>Time Tracking - Enter Time Form</h1>
+
+        <button
+            // import style here once its made
+            onClick = {handleCancel}
+            aria-label="Cancel"
+        >
+        </button>
+ 
+        <button
+            // import style here once its made
+            onClick = {handleSubmit}
+            aria-label="Submit"
+        >
+
+        </button>
+
+        </div>
+    );
+
+};
