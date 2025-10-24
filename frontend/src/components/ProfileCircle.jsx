4c81768bc05f3d79c159d18b0e1675678830e02e
torimcclelland <tori9991@hotmail.com>
2025-10-24 12:31:24 -0400

    profile circle for user avatar

---
+++ frontend/src/components/ProfileCircle.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Circle = styled.div`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border-radius: 50%;
+  overflow: hidden;
+  display: inline-block;
+  background-color: #f0f0f0;
+`;
+
+const Avatar = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const ProfileCircle = ({ avatarUrl, size = 48, alt = 'User avatar' }) => {
+  return (
+    <Circle size={size}>
+      <Avatar src={avatarUrl} alt={alt} />
+    </Circle>
+  );
+};
+
+ProfileCircle.propTypes = {
+  avatarUrl: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  alt: PropTypes.string,
+};
+
+export default ProfileCircle;
